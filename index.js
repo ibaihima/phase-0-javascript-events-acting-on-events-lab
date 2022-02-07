efc4b1878199cc35861c4afc7eb499a17d96874a
Ibaihima <ibaihima@hotmail.com>
2022-02-07 11:03:15 -0500

    commit tree ec79b055021b4848c19e4efbd68cbe962e029e0e

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ function moveDodgerLeft() {
     const left = parseInt(leftNumbers, 10);
   
     if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
+      dodger.style.left = `${left - 4}px`;
     }
 }
 function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace('px', '');
     const left = parseInt(leftNumbers, 10)
     if (left < 360) {
-      dodger.style.left = `${left + 1}px`
+      dodger.style.left = `${left + 4}px`
     }
   }
 document.addEventListener('keydown', function(e) {
